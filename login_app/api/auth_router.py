121a7e39cdc930e17f16f473b5090492999b2578
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-08-21 19:49:22 +0300

    Add refresh token method

--- login_app/api/auth_router.py
+++ login_app/api/auth_router.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm,HTTPBearer, HTTPAuthorizationCredentials
 
-from login_app.api.auth_services import get_current_user, register_user, login_user, logout_user, refresh_token
+from login_app.api.auth_services import get_current_user, register_user, login_user, logout_user, refresh_access_token_service,refresh_refresh_token_service
 
 user_dependency = Annotated[dict,Depends(get_current_user)]
+security = HTTPBearer()
 auth_router = APIRouter(prefix='')
 
 
@@ -21,5 +22,10 @@ async def logout(user:user_dependency):
     return await logout_user(user)
 
 @auth_router.put('/refresh')
-async def refresh(token):
-    return await refresh_token(token)
+async def refresh_access_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    return await refresh_access_token_service(credentials.credentials)
+
+@auth_router.put('/refresh')
+async def refresh_refresh_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    return await refresh_refresh_token_service(credentials.credentials)
+
--- login_app/api/auth_services.py
+++ login_app/api/auth_services.py
@@ -9,7 +9,8 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from login_app.utils.query_services import register_service, authenticate_user
 
-TOKEN_EXPIRATION = 3600
+ACCESS_TOKEN_EXPIRATION = 15
+REFRESH_TOKEN_EXPIRATION = 14400
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 load_dotenv()
@@ -20,14 +21,18 @@ logged_users = {}
 async def login_user(email: str, password: str):
     user = await authenticate_user(email, password)
 
-    token = await create_access_token({'user_id': user[0][0],
+    access_token = await create_access_token({'user_id': user[0][0],
                                   'email': user[0][1],
                                   'role': user[0][3]})
+
+    refresh_token = await create_refresh_token()
+
 
     logged_users.update({f"{user[0][0]}": {'Email': email}})
 
     return {
-        "access_token": token,
+        "access_token": access_token,
+        'refresh_token': refresh_token,
         "token_type": "bearer",
         "role": user[0][2]
     }
@@ -48,16 +53,23 @@ async def register_user(email: str, password: str):
 
 
 async def create_access_token(data: dict):
-    data_to_encode = data.copy()  # Shallow copy to avoid modifying the original
-    expiration = datetime.now() + timedelta(minutes=TOKEN_EXPIRATION)
+    data_to_encode = data.copy()
+    expiration = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRATION)
     data_to_encode.update({'exp': expiration, 'last_activity': datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")})
-    print(data_to_encode['exp'])
-
     secret_key = os.getenv('SECRET_KEY')
     algorithm = os.getenv('ALGORITHM')
     encode_jwt = jwt.encode(data_to_encode, secret_key, algorithm)
-    print(encode_jwt)
     return encode_jwt
+
+async def create_refresh_token():
+    data = {}
+    expiration = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRATION)
+    data.update({'exp': expiration, 'last_activity': datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")})
+    secret_key = os.getenv('SECRET_KEY')
+    algorithm = os.getenv('ALGORITHM')
+    encode_jwt = jwt.encode(data, secret_key, algorithm)
+    return encode_jwt
+
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
@@ -98,10 +110,19 @@ async def decode_token(token):
     except Exception as e:
         print(f"Error decoding JWT: {e}")
 
-async def refresh_token(token):
+
+async def refresh_access_token_service(token):
 
     decoded_payload = await decode_token(token)
     if decoded_payload:
         return  await create_access_token(decoded_payload)
     else:
         raise ValueError("Invalid token")
+
+async def refresh_refresh_token_service(token):
+    decoded_payload = await decode_token(token)
+    if decoded_payload:
+        return await create_refresh_token()
+    else:
+        raise ValueError("Invalid token")
+
