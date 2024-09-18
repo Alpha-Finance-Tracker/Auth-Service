f68820893e44b7907a308b5a50ad8fd5ebf0e627
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-09-18 14:10:59 +0300

    CI

--- login_app/database/database.py
+++ login_app/database/database.py
@@ -4,10 +4,14 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from login_app.database.config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
+if DATABASE_URL:
+    engine = create_async_engine(DATABASE_URL, echo=True)
+    SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+else:
+    engine = None
+    SessionLocal = None
 
+Base = declarative_base()
 
 async def get_db():
     async with SessionLocal() as db:
